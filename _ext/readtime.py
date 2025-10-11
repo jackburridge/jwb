37b115cc28bb7d605e9e2f74f866f5ac0283ab8d
jack.burridge <jack.burridge@mail.com>
2025-10-11 22:47:20 +0100

    docs: move read time to its own extension

---
+++ _ext/readtime.py
@@ -0,0 +1,62 @@
+import math
+import re
+from typing import Any, Optional
+
+from docutils import nodes
+from sphinx.transforms import SphinxTransform
+from sphinx.util.matching import patmatch
+
+
+def has_read_time(node: nodes.Node) -> bool:
+
+    for paragraph in node.findall(nodes.paragraph):
+        if "read-time" in paragraph["classes"]:
+            return True
+    return False
+
+
+def find_title(node: nodes.Node) -> Optional[nodes.title]:
+    for title in node.findall(nodes.title):
+        return title
+    return None
+
+
+WORD_PATTERN = re.compile(r"[A-Z]+[a-z]*|[a-z]+|\d+")
+
+
+class AddReadTime(SphinxTransform):
+    default_priority = 200
+
+    def apply(self, **kwargs: Any) -> None:
+
+        docname = self.env.path2doc(self.document["source"])
+        if docname is None:
+            return
+
+        exclude = any(
+            [patmatch(docname, p) for p in self.config.readtime_exclude_patterns]
+        )
+        include = not exclude and any(
+            [patmatch(docname, p) for p in self.config.readtime_include_patterns]
+        )
+        if include and not has_read_time(self.document):
+            title = find_title(self.document)
+            if title is not None:
+                title_index = title.parent.index(title)
+
+                words = WORD_PATTERN.findall(self.document.astext())
+                read_time = math.ceil(len(words) / 200)
+
+                paragraph = nodes.paragraph("", nodes.Text(f"{read_time} min read"))
+
+                paragraph["classes"] = ["readtime"]
+                title.parent.insert(title_index + 1, paragraph)
+
+
+def setup(app):
+    app.add_config_value("readtime_include_patterns", [], "env")
+    app.add_config_value("readtime_exclude_patterns", [], "env")
+
+    app.add_transform(AddReadTime)
+
+    return {}
--- conf.py
+++ conf.py
@@ -1,28 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-import math
-import re
-from http.server import SimpleHTTPRequestHandler
+import os
+import sys
 from typing import Any
 
-from ablog.post import PostNode
 from docutils import nodes
 from docutils.nodes import TextElement
 from sphinx.transforms import SphinxTransform
 from sphinx.transforms.post_transforms.code import HighlightLanguageTransform
 
+
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath("_ext"))
 
 
 # -- Project information -----------------------------------------------------
@@ -35,36 +23,42 @@ html_title = "Jack Burridge"
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "ablog",
     "sphinx_copybutton",
     "sphinx_togglebutton",
     "sphinx.ext.intersphinx",
+    "readtime",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", ".venv"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML 'Thumbs.db', '.DS_Store', Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "pydata_sphinx_theme"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_theme_options = {
+    "logo": {
+        "text": "Jack Burridge",
+        "image_light": "_static/logo.jpg",
+        "image_dark": "_static/logo.jpg",
+    },
+    "footer_start": [],
+    "footer_end": [],
+    "search_bar_text": "Search...",
+}
+html_css_files = [
+    "css/custom.css",
+]
+html_sidebars = {
+    "blog": ["ablog/categories.html", "ablog/tagcloud.html", "ablog/archives.html"],
+    "blog/**": ["ablog/postcard.html", "ablog/recentposts.html", "ablog/archives.html"],
+}
+html_favicon = "_static/favicon.ico"
 
 
 # -- ABlog ---------------------------------------------------
@@ -79,37 +73,14 @@ post_auto_image = 1
 post_auto_excerpt = 2
 
 
-html_theme_options = {
-    "logo": {
-        "text": "Jack Burridge",
-        "image_light": "_static/logo.jpg",
-        "image_dark": "_static/logo.jpg",
-    },
-    "footer_start": [],
-    "footer_end": [],
-    "search_bar_text": "Search...",
-}
-
-
-html_css_files = [
-    "css/custom.css",
-]
-
-
-html_sidebars = {
-    # "index": ["hello.html"],
-    # "about": ["hello.html"],
-    # "publications": ["hello.html"],
-    # "projects": ["hello.html"],
-    # "talks": ["hello.html"],
-    "blog": ["ablog/categories.html", "ablog/tagcloud.html", "ablog/archives.html"],
-    "blog/**": ["ablog/postcard.html", "ablog/recentposts.html", "ablog/archives.html"],
-}
-
-
-html_favicon = "_static/favicon.ico"
+# -- intersphinx ---------------------------------------------
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+# -- readtime -----------------------------------------------
+
+
+readtime_include_patterns = ["blog/*/*"]
 
 
 class TrimWhitespaceTransform(SphinxTransform):
@@ -127,34 +98,7 @@ class TrimWhitespaceTransform(SphinxTransform):
         node[:] = [nodes.Text(source)]
 
 
-def has_read_time(node: nodes.Node) -> bool:
-    for paragraph in node.findall(nodes.paragraph):
-        if "read-time" in paragraph["classes"]:
-            return True
-    return False
-
-
-class AddReadTime(SphinxTransform):
-    default_priority = 800
-
-    def apply(self, **kwargs: Any) -> None:
-        for post_node in self.document.findall(PostNode):
-            parent = post_node.parent
-            title_index = post_node.parent.first_child_matching_class(nodes.title)
-            if title_index is not None:
-                if not has_read_time(parent):
-                    words = re.findall(r"[a-zA-Z0-9]+", parent.astext())
-                    read_time = math.ceil(len(words) / 200)
-
-                    paragraph = nodes.paragraph("", nodes.Text(f"{read_time} min read"))
-
-                    paragraph["classes"] = ["read-time"]
-                    parent.insert(title_index + 1, paragraph)
-
-
 def setup(app):
     app.add_transform(TrimWhitespaceTransform)
-
-    app.add_transform(AddReadTime)
 
     return {}
